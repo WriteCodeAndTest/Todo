86dde45069c1de3cba56dc135aa66ebd01a29399
Ligalaiz <slavakuz88@gmail.com>
2022-02-12 20:55:50 +0300

    feat: implement test for TodoApp component

--- tests/components/App.test.tsx
+++ tests/components/App.test.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from '../../src/components/App';
 
-it('Test', () => {
-  const { getByTestId } = render(<App />);
-  screen.debug();
-  expect(getByTestId('wrapper')).toHaveTextContent('Test test');
+describe('TodoApp', () => {
+  it('Render TodoApp component', () => {
+    const { getByTestId } = render(<App />);
+    screen.debug();
+    expect(getByTestId('todoAdd')).toBeInTheDocument();
+    expect(getByTestId('input')).toBeInTheDocument();
+    expect(getByTestId('btn')).toBeInTheDocument();
+  });
 });

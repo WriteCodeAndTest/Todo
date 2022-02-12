0356cdecc57628e5805e3e734778f6f79803e161
Ligalaiz <slavakuz88@gmail.com>
2022-02-12 16:40:29 +0300

    refactor: change path for test

--- tests/components/App.test.tsx
+++ tests/components/App.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { App } from '../../src/App';
+import { App } from '../../src/components/App';
 
 it('Test', () => {
   const { getByTestId } = render(<App />);

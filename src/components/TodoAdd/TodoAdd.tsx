8364826606a85d278670a78bb37b8a99cb823f85
Ligalaiz <slavakuz88@gmail.com>
2022-02-12 21:01:38 +0300

    refactor: add data from store

--- src/components/TodoAdd/TodoAdd.tsx
+++ src/components/TodoAdd/TodoAdd.tsx
@@ -1,17 +1,34 @@
 import React, { FC } from 'react';
+import { TodoStore } from '@src/store';
+import { observer } from 'mobx-react';
 import { todoAdd } from './TodoAddStyle';
 import { Btn } from '../Btn';
 import { Input } from '../Input';
+import { v4 as uuid } from 'uuid';
 
-const TodoAdd: FC = () => {
+const TodoAdd: FC = observer(() => {
+  const { title, setTodos } = TodoStore;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodos({
+      title,
+      data: new Date().toLocaleDateString(),
+      status: false,
+      notice: false,
+      id: uuid(),
+    });
+  };
+
   return (
-    <form css={todoAdd}>
-      <Input placeholder="What needs to be done?" name="todoAdd" />
-      <Btn type="submit" styleBtn="manage">
-        Add
-      </Btn>
-    </form>
+    <>
+      <form data-testid="todoAdd" css={todoAdd} onSubmit={handleSubmit}>
+        <Input placeholder="What needs to be done?" name="todoAdd" />
+        <Btn type="submit" styleBtn="manage">
+          Add
+        </Btn>
+      </form>
+    </>
   );
-};
+});
 
 export { TodoAdd };

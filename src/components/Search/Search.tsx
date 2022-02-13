013cb432f27960db9d439bf9227417fb4f3914d2
Ligalaiz <slavakuz88@gmail.com>
2022-02-13 21:53:39 +0300

    feat: implement search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,69 @@
+import React, { FC, KeyboardEvent, ChangeEvent, MouseEvent } from 'react';
+import { TodoStore } from '@src/store';
+import { observer } from 'mobx-react';
+import { Input } from '../Input';
+import { Btn } from '../Btn';
+import { search, searchField, searchFilter } from './SearchStyle';
+
+const Search: FC = observer(() => {
+  const { query, setQuery, sortType, setSortType, searchRequest } = TodoStore;
+
+  const handleKey = (e: KeyboardEvent<HTMLInputElement>) =>
+    e.key === 'Enter' ? searchRequest(query) : undefined;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setQuery(e.target.value);
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const { name } = e.currentTarget;
+    setSortType(name);
+    searchRequest(query);
+  };
+
+  return (
+    <div data-testid="search" css={search}>
+      <Input
+        placeholder="type to search"
+        css={searchField}
+        name="search"
+        type="text"
+        handleKey={handleKey}
+        handleChange={handleChange}
+        value={query}
+      />
+      <div css={searchFilter}>
+        <Btn
+          handleClick={handleClick}
+          styleBtn="manage"
+          name="all"
+          data-testid="allBtn"
+          isActive={sortType === 'all'}
+        >
+          All
+        </Btn>
+
+        <Btn
+          handleClick={handleClick}
+          styleBtn="manage"
+          name="active"
+          data-testid="activeBtn"
+          isActive={sortType === 'active'}
+        >
+          Active
+        </Btn>
+
+        <Btn
+          handleClick={handleClick}
+          styleBtn="manage"
+          name="done"
+          data-testid="doneBtn"
+          isActive={sortType === 'done'}
+        >
+          Done
+        </Btn>
+      </div>
+    </div>
+  );
+});
+
+export { Search };

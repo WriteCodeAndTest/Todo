340f06e7a51e74f157b99f5142b4a062c3b997c6
Ligalaiz <slavakuz88@gmail.com>
2022-02-13 22:05:33 +0300

    feat: implement methods

--- src/store/todo.store.ts
+++ src/store/todo.store.ts
@@ -8,14 +8,65 @@ class Store {
 
   todos: ITodos[] = [];
 
+  renderTodos: ITodos[] = [];
+
   title = '';
 
   setTitle = (value: string) => {
     this.title = value;
   };
 
+  query = '';
+
+  setQuery = (value: string) => {
+    this.query = value;
+  };
+
+  sortType = 'all';
+
+  setSortType = (value: string) => {
+    this.sortType = value;
+  };
+
+  deleteTodo = (id: string) => {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+    this.renderTodos = this.todos;
+  };
+
+  markTodo = (id: string) => {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { ...todo, mark: !todo.mark } : todo,
+    );
+    this.renderTodos = this.todos;
+  };
+
+  setStatus = (id: string) => {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { ...todo, status: !todo.status } : todo,
+    );
+    this.renderTodos = this.todos;
+  };
+
+  searchRequest = (value: string) => {
+    const reg = new RegExp(value, 'gi');
+
+    if (this.sortType === 'active') {
+      this.renderTodos = this.todos
+        .filter((todo) => !todo.status)
+        .filter((todo) => todo.title.match(reg));
+    } else if (this.sortType === 'done') {
+      this.renderTodos = this.todos
+        .filter((todo) => todo.status)
+        .filter((todo) => todo.title.match(reg));
+    } else {
+      this.renderTodos = this.todos.filter((todo) => todo.title.match(reg));
+    }
+  };
+
   setTodos = (value: ITodos) => {
-    this.todos.push(value);
+    this.todos.unshift(value);
+    this.renderTodos = this.todos;
+    this.title = '';
   };
 }
 

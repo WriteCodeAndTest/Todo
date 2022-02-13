df287faf8613f9857dca16f234facf04f2312684
Ligalaiz <slavakuz88@gmail.com>
2022-02-13 21:54:57 +0300

    feat: add styles for Search component

---
+++ src/components/Search/SearchStyle.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+export const search = css`
+  display: flex;
+  margin-bottom: 15px;
+`;
+
+export const searchFilter = css`
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+`;
+
+export const searchField = css`
+  margin-right: 5px;
+`;

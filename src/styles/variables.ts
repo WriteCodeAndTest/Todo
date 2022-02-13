e1ad18ba7aa4b9bc77f9c09bc9b64e3c404f2338
Ligalaiz <slavakuz88@gmail.com>
2022-02-13 21:58:59 +0300

    refactor: change global color

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -7,14 +7,16 @@ export const color: IColor = {
   'green-color': '#26851b',
   'blue-color': '#3b43b3',
   'gray-color': '#9e9e9e',
+  'red-color--light': '#ffe5e5',
+  'black-color--light': '#d9d9e1',
+  primary: '#D43728',
 
   'gray-color--light': '#DDDDDD',
 
   'black-color': '#222222',
-  'black-color--light': '#00000080',
+
   'black-color--lightX0': '#00000018',
   'black-color--lightX1': '#00000005',
 
-  primary: '#D43728',
   'primary-dark': '#ff00005e',
 };

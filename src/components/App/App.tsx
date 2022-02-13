045bd628f15bd262e64a6fa2a149e859de92d580
Ligalaiz <slavakuz88@gmail.com>
2022-02-13 22:07:07 +0300

    refactor: add new components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,25 @@
 import React, { FC } from 'react';
 import { TodoAdd } from '@components/TodoAdd';
-import { app } from './AppStyle';
+import { Search } from '@components/Search';
+import { TodoList } from '@components/TodoList';
+import { TodoStore } from '@src/store';
+import { observer } from 'mobx-react';
+import { app, title, todosCount } from './AppStyle';
 
-const App: FC<React.ReactNode> = () => {
+const App: FC<React.ReactNode> = observer(() => {
+  const { todos } = TodoStore;
+
   return (
     <div css={app}>
+      <div css={title}>
+        <h1>Todo list</h1>
+        <p css={todosCount}>{todos.length} more to do, 0 done</p>
+      </div>
       <TodoAdd />
+      <Search />
+      <TodoList />
     </div>
   );
-};
+});
 
 export { App };

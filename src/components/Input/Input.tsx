cd24c03d9d8add9f084dc0c41239b569e3b8fb06
Ligalaiz <slavakuz88@gmail.com>
2022-02-13 22:02:15 +0300

    feat: implement handler for input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,35 +1,24 @@
 import React, { FC } from 'react';
 import { observer } from 'mobx-react';
 import { input } from './InputStyle';
-import { TodoStore } from '@src/store';
 import { IInput } from '@src/interfaces';
-import { v4 as uuid } from 'uuid';
 
-const Input: FC<IInput> = observer(({ placeholder, name }) => {
-  const { title, setTitle, setTodos } = TodoStore;
-
-  return (
-    <input
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      onKeyDown={(e) =>
-        e.key === 'Enter'
-          ? setTodos({
-              title,
-              data: new Date().toLocaleDateString(),
-              status: false,
-              notice: false,
-              id: uuid(),
-            })
-          : undefined
-      }
-      data-testid="input"
-      css={input}
-      type="text"
-      name={name}
-      placeholder={placeholder}
-    />
-  );
-});
+const Input: FC<IInput> = observer(
+  ({ value, placeholder, name, handleKey, handleChange, className }) => {
+    return (
+      <input
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKey}
+        data-testid="input"
+        css={input}
+        type="text"
+        name={name}
+        placeholder={placeholder}
+        className={className}
+      />
+    );
+  },
+);
 
 export { Input };

d66df406fa930db2b182b26678ddd28b39479f6f
Ligalaiz <slavakuz88@gmail.com>
2022-02-14 12:05:07 +0300

    refactor:  make testid optional

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,13 +4,21 @@ import { input } from './InputStyle';
 import { IInput } from '@src/interfaces';
 
 const Input: FC<IInput> = observer(
-  ({ value, placeholder, name, handleKey, handleChange, className }) => {
+  ({
+    value,
+    placeholder,
+    name,
+    handleKey,
+    handleChange,
+    className,
+    testid,
+  }) => {
     return (
       <input
         value={value}
         onChange={handleChange}
         onKeyDown={handleKey}
-        data-testid="input"
+        data-testid={testid}
         css={input}
         type="text"
         name={name}

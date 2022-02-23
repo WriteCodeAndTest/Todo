dcdd1421b77ffe12650501e6d00fe8154dc2edba
Ligalaiz <slavakuz88@gmail.com>
2022-02-23 14:15:47 +0300

    refactor: change delay util test name

--- tests/utils/delay.test.ts
+++ tests/utils/delay.test.ts
@@ -2,13 +2,15 @@ import { delay } from '../../client/src/utils';
 
 jest.useFakeTimers();
 
-test('delay on 1 second', () => {
-  const callback = jest.fn();
+describe('Delay function', () => {
+  it('delay function check was called after 1 second', () => {
+    const callback = jest.fn();
 
-  delay(1000, callback);
+    delay(1000, callback);
 
-  jest.advanceTimersByTime(1000);
+    jest.advanceTimersByTime(1000);
 
-  expect(callback).toBeCalled();
-  expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
 });

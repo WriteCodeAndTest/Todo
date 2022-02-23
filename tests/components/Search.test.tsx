1d9ef31ccb71551dd8fe56e4f3464c7ec9a6552b
Ligalaiz <slavakuz88@gmail.com>
2022-02-23 14:12:22 +0300

    refactor: change Search component tests name

--- tests/components/Search.test.tsx
+++ tests/components/Search.test.tsx
@@ -6,35 +6,40 @@ import { render } from '@testing-library/react';
 import { App } from '../../client/src/components/App';
 import { TodoStore } from '../../client/src/store';
 
-describe('Search', () => {
-  it('component render check', () => {
-    const { getByTestId } = render(<App />);
+describe('Search component', () => {
+  describe('Component exist', () => {
+    it('searchField check that the component has been rendered #smoke', () => {
+      const { getByTestId } = render(<App />);
 
-    expect(getByTestId('search')).toBeInTheDocument();
-    expect(getByTestId('searchField')).toBeInTheDocument();
-    expect(getByTestId('allBtn')).toBeInTheDocument();
-    expect(getByTestId('activeBtn')).toBeInTheDocument();
-    expect(getByTestId('doneBtn')).toBeInTheDocument();
+      expect(getByTestId('search')).toBeInTheDocument();
+      expect(getByTestId('searchField')).toBeInTheDocument();
+      expect(getByTestId('allBtn')).toBeInTheDocument();
+      expect(getByTestId('activeBtn')).toBeInTheDocument();
+      expect(getByTestId('doneBtn')).toBeInTheDocument();
+    });
   });
 
-  it('check input field', () => {
-    TodoStore.setTodosTest({
-      title: 'Todo content',
-      data: new Date().toLocaleDateString(),
-      status: false,
-      mark: false,
-      id: '123',
+  describe('Сomponent works', () => {
+    it('searchField check change handler was called after type', () => {
+      TodoStore.setTodosTest({
+        title: 'Todo content',
+        data: new Date().toLocaleDateString(),
+        status: false,
+        mark: false,
+        id: '123',
+      });
+
+      const { getByTestId, queryByText } = render(<App />);
+      const searchField = getByTestId('searchField');
+
+      userEvent.type(searchField, 'Test');
+      expect(searchField).toHaveValue('Test');
+
+      userEvent.type(searchField, '{alt}');
+      expect(queryByText('Todo content')).toBeInTheDocument();
+
+      userEvent.type(searchField, '{enter}');
+      expect(queryByText('Todo content')).toBeNull();
     });
-    const { getByTestId, queryByText } = render(<App />);
-    const searchField = getByTestId('searchField');
-
-    userEvent.type(searchField, 'Test');
-    expect(searchField).toHaveValue('Test');
-
-    userEvent.type(searchField, '{alt}');
-    expect(queryByText('Todo content')).toBeInTheDocument();
-
-    userEvent.type(searchField, '{enter}');
-    expect(queryByText('Todo content')).toBeNull();
   });
 });

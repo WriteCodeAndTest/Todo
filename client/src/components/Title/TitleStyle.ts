34e3d7b91b9840491ad5e6c5e3928cddf230f568
Ligalaiz <slavakuz88@gmail.com>
2022-02-23 18:21:23 +0300

    feat: add styles for Title component

---
+++ client/src/components/Title/TitleStyle.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const title = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+`;
+
+export const todosCount = css`
+  font-size: 20px;
+`;

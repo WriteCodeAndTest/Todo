b6c6584221b0db62d8c2e42f60e0d6c2f1e1275b
Ligalaiz <slavakuz88@gmail.com>
2022-02-23 14:16:08 +0300

    refactor: change storage util test name

--- tests/utils/storage.test.ts
+++ tests/utils/storage.test.ts
@@ -1,15 +1,24 @@
 import { get, set, del } from '../../client/src/utils/storage.utils';
 
-describe('storageUtils', () => {
-  it('return data from localStorage', () => {
-    const KEY = 'foo';
-    const VALUE = 'bar';
-    get(KEY);
+const KEY = 'foo';
+const VALUE = 'bar';
+
+describe('Storage functions', () => {
+  it('set check set data to localStorage #smoke', () => {
     set(KEY, VALUE);
-    del(KEY);
+
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY, `${'"bar"'}`);
+  });
+
+  it('get check get data from localStorage #smoke', () => {
+    get(KEY);
 
     expect(localStorage.getItem).toHaveBeenLastCalledWith(KEY);
-    expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY, `${'"bar"'}`);
+  });
+
+  it('del check delete data from localStorage #smoke', () => {
+    del(KEY);
+
     expect(localStorage.removeItem).toHaveBeenLastCalledWith(KEY);
   });
 });
